5db59e77cbfc38ce61a2c0aba2da58807e0c7ed8
Randy LeVeque <rjl@uw.edu>
2026-01-07 08:15:19 -0800

    add runs/RC300_multi with code to do parameter sweep in h0

---
+++ crater_code/5eqns_transport/mfrk2Dev/runs/RC300_multi/README.txt
@@ -0,0 +1,43 @@
+
+The command
+    python runclaw_makeplots_h0vals.py <nprocs>
+
+will run mfclaw for a set of ocean depths h0 with all other parameters fixed
+and create separate output directories and plot directories for each,
+as subdirectories of ./mfclaw_outputs and ./mfclaw_plots.
+
+<nprocs> should be an integer indicating how many jobs to run in parallel.
+
+Some things to adjust in this script before running this:
+
+    Set dry_run to True initially,
+        or to False to actually do runs and/or make plots
+
+    Set run_code and make_plots
+        to indicate what should be done (when dry_run==False)
+
+    Set xmfclaw_path to the path to the executable xamr (after compiling)
+
+    Set h0vals to the desired set of h0 values (currently [4000, 3000])
+
+See the comments in runclaw_makeplots_h0vals.py for more details.
+
+------------
+Other files:
+
+make_cases.py 
+    contains make_all_cases_h0, 
+    which makes the caselist (list of case dictionaries).
+    Note that it loops over h0vals but hard-wires the other case parameters
+    'xupper' and 'yupper', which are also used in setrun_case.py
+    (A version could be created to loop over one of these instead of h0)
+
+setrun_case.py 
+    contains setrun with an argument `case` that will be
+    passed in when running a particular case.
+
+setplot_case.py 
+    contains setplot with an argument `case` that will be
+    passed in when running a particular case.
+
+
---
+++ crater_code/5eqns_transport/mfrk2Dev/runs/RC300_multi/make_cases.py
@@ -0,0 +1,78 @@
+
+
+def make_all_cases_h0(runs_dir, xmfclaw_path, make_plots, h0vals):
+    """
+    Output: *caselist*, a list of cases to be run.
+    Each case should be dictionary of any parameters needed to set up an
+    individual case.  These will be used by run_one_case.
+
+    For this example, the ocean depth h0 is varied.
+
+    If xmfclaw_path is not None, AMRClaw will be run for each of
+    the h0 values, based on code in setrun_case.py.
+    The setrun function in this directory should take an argument case
+    so that parameters for each case can be passed in.
+
+    If make_plots is True, plotting will also be done based on code in
+    setplot_case.py.
+    The setplot function in this directory should take an argument case
+    so that parameters for each case can be passed in.
+
+    A unique directory will be created for each run, with names
+    based on h0, and residing within runs_dir.
+    The _output and _plots directories will be within the run directory.
+
+    xupper, yupper are hard-wired to fixed values below, but if a list
+    of values is to be tested there could also be loops on these.
+    """
+    import os
+
+    # Create a list of the cases to be run:
+    caselist = []
+
+    for h0 in h0vals:
+
+        # Create all directories needed first, in case there's a problem:
+
+        # name for each run e.g. h0_0900 if h=900. (depth as 0-padded integer)
+        run_name = f'h0_{h0:04d}'
+
+        runs_dir = os.path.abspath(runs_dir)
+        outdir = os.path.join(runs_dir, 'mfclaw_outputs/_output_%s' \
+                    % run_name)
+        plotdir = os.path.join(runs_dir, 'mfclaw_plots/_plots_%s' \
+                    % run_name)
+        os.system('mkdir -p %s' % outdir)
+        print('Created %s' % outdir)
+        os.system('mkdir -p %s' % plotdir)
+        print('Created %s' % plotdir)
+
+        case = {}
+
+        case['case_name'] = f'depth{h0:.0f}'
+        case['outdir'] = outdir
+
+        case['xclawcmd'] = xmfclaw_path   # executable created by 'make .exe'
+                                          # or None, in which case code is
+                                          # not run (plots may be made below)
+
+        # setrun parameters:
+        case['setrun_file'] = 'setrun_case.py'
+
+        # parameter(s) that vary:
+        case['h0'] = h0
+        case['xupper'] = 20e3  # fixed for this set of cases
+        case['yupper'] = 4e3   # fixed for this set of cases
+
+        if make_plots:
+            case['plotdir'] = plotdir
+        else:
+            case['plotdir'] = None  # if None, will not make plots
+
+        case['setplot_file'] = 'setplot_case.py'
+
+        #case['redirect_python'] = False  # for debugging send to stdout
+
+        caselist.append(case)
+
+    return caselist
